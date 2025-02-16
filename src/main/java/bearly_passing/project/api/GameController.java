54091290f596e2d9eca988aa40cf96c0ca7f33f4
Kevin <kevfritz1@gmail.com>
2025-02-16 15:33:34 -0600

    Kevin: Saving before pulling

--- src/main/java/bearly_passing/project/api/GameController.java
+++ src/main/java/bearly_passing/project/api/GameController.java
@@ -1,7 +1,41 @@
 package bearly_passing.project.api;
+
+import bearly_passing.project.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class GameController {
+
+    private UserService userService;
+
+    @Autowired
+    public GameController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public enum GameType {
+        MATCHING, AUDIO, QUIZ
+    }
+
+    // Return list of game types
+    // @GetMapping("/game")
+    // public String home() {
+    //     return "home";
+    // }
+    
+    // Return list of game types
+    // @GetMapping("/game")
+    @RequestMapping(value = "/game/{ID}/{TYPE}", method = RequestMethod.POST )
+    public ResponseEntity gamePlay(@PathVariable(value="ID") long studysetID, @PathVariable(value="TYPE") String gameType) {
+        return new ResponseEntity(userService.takeQuiz(studysetID, gameType), HttpStatus.OK);
+    }
 
 }
--- src/main/java/bearly_passing/project/services/UserService.java
+++ src/main/java/bearly_passing/project/services/UserService.java
@@ -6,15 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import bearly_passing.project.data.UserRepository;
+import bearly_passing.project.domain.Question;
 import bearly_passing.project.domain.Student;
 import bearly_passing.project.domain.Teacher;
 import bearly_passing.project.domain.User;
 import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
+
+    @PersistenceContext
+    private EntityManager em;
 
     public <T extends User> T createUser(T user) {
         return userRepository.save(user);
@@ -40,4 +48,13 @@ public class UserService {
 
         return teacher;
     }
+
+    @Transactional
+    public List<Question> takeQuiz(long studysetID, String gameType) {
+        return em.createQuery("SELECT g.studyset_id FROM Game g WHERE g.studyset_id = :id AND g.type = :type", Question.class)
+            .setParameter("id", studysetID)
+            .setParameter("type", gameType)
+            .getResultList().get(0);
+    }
+
 }

918455281dcf206765cfb02ed331680fdb4d0628
household-four <barringerchristen4@gmail.com>
2025-02-16 13:25:10 -0800

    added admin/populate endpoint

--- src/main/java/bearly_passing/project/api/AdminController.java
+++ src/main/java/bearly_passing/project/api/AdminController.java
@@ -3,12 +3,14 @@ package bearly_passing.project.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bearly_passing.project.domain.User;
 import bearly_passing.project.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/admin")
@@ -20,6 +22,12 @@ public class AdminController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @PostMapping("/populate")
+    public ResponseEntity<String> populateDummyData() {
+        userService.populateDummyData();
+        return ResponseEntity.ok("Dummy data populated successfully!");
     }
 
 }
